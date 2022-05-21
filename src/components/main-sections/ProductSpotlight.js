743c2bde3f99471f3c0433614bd167599272a294
iamthequeen <88898113+iamthequeen@users.noreply.github.com>
2022-05-21 00:49:21 -0500

    added local images for logos and blog posts

--- src/components/main-sections/ProductSpotlight.js
+++ src/components/main-sections/ProductSpotlight.js
@@ -7,9 +7,9 @@ function ProductSpotlight() {
       <div className="two-cols">
         
         <div className="col">
-          <img src="https://images.unsplash.com/photo-1557551541-ba7165b5f764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" className="teapot-coffee-img"/>
-          <img src="" alt=""/>
-          <div className="pointer"></div>
+          <img src={"images/heroImg.jpg"} alt="" className="teapot-coffee-img"/>
+          {/* <img src="" alt=""/>
+          <div className="pointer"></div> */}
         </div>
         
         <div className="col">
